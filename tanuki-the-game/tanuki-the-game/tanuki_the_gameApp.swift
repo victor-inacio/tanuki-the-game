c32965d28baaa92f7509974087f2c0d2a765859e
Victor Inácio <42353254+victor-inacio@users.noreply.github.com>
2024-03-08 16:05:13 -0300

    Installed XCode Project

---
+++ tanuki-the-game/tanuki-the-game/tanuki_the_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  tanuki_the_gameApp.swift
+//  tanuki-the-game
+//
+//  Created by Victor Soares on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct tanuki_the_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
