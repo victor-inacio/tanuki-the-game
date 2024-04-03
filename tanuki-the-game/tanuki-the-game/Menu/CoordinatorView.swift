ed5e3760b3c96ec8584096e3c987120b20ef3673
Luca <paesberaldiluca@gmail.com>
2024-04-03 15:08:25 -0300

    Merge branch 'main' into menu

--- tanuki-the-game/tanuki-the-game/Menu/CoordinatorView.swift
+++ tanuki-the-game/tanuki-the-game/Menu/CoordinatorView.swift
@@ -8,22 +8,23 @@
 import SwiftUI
 
 struct CoordinatorView: View {
-    
-    @State private var coordinator = Coordinator()
-    
-    var body: some View {
-        NavigationStack(path: $coordinator.path) {
-            coordinator.build(page: .mainMenu)
-                .navigationDestination(for: Page.self) { page in
-                    coordinator.build(page: page)
-                }
-                .fullScreenCover(item: $coordinator.fullScreenCover) {
-                    fullScreenCover in
-                    coordinator.build(fullScreenCover: fullScreenCover)
-                }
+  @State private var coordinator = Coordinator()
+
+  var body: some View {
+    NavigationStack(path: $coordinator.path) {
+      coordinator.build(page: .mainMenu)
+        .navigationDestination(for: Page.self) { page in
+          coordinator.build(page: page)
+                .transition(.opacity.animation(.easeIn)) // Or other transition types like .move, .opacity, etc.
+            //WHY ISN'T THE ANIMATION WORKING OH MY GOOOOOOOOOOOOODDDD
         }
-        .environment(coordinator)
+        .fullScreenCover(item: $coordinator.fullScreenCover) {
+          fullScreenCover in
+            coordinator.build(fullScreenCover: fullScreenCover)
+        }
     }
+    .environment(coordinator)
+  }
 }
 
 #Preview {
