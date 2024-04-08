673a81e55c9cdf0fd2a4d482d9d4b3332c04c9d2
Luca <paesberaldiluca@gmail.com>
2024-04-08 16:01:30 -0300

    Changed some stuff to test the presentPopup function back into the game

--- tanuki-the-game/tanuki-the-game/App/GameViewController.swift
+++ tanuki-the-game/tanuki-the-game/App/GameViewController.swift
@@ -7,9 +7,12 @@
 
 import UIKit
 import SceneKit
+import SwiftUI
 
 class GameViewController: UIViewController {
 
+    @ObservedObject private var presentationViewModel = PresentationViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,5 +38,22 @@ class GameViewController: UIViewController {
         scene.background.contents = UIColor.black
         
         view.addSubview(scnView)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear")
+//        presentPopup()
+    }
+    
+    
+    //this NEEDS to be fixed, View currently can't be dismissed.
+    func presentPopup() {
+        let viewModel = DrawingViewModel() // Create view model instance
+        let contentView = DrawingView(currentView: .constant(.drawingView), viewModel: viewModel, presentationViewModel: presentationViewModel) // Pass the presentationViewModel
+        
+        // Present the ContentView as a popup
+        let popupViewController = UIHostingController(rootView: contentView)
+        popupViewController.modalPresentationStyle = .overFullScreen // Ensure transparent background
+        present(popupViewController, animated: true, completion: nil)
     }
 }
