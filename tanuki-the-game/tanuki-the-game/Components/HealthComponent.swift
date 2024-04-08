5af5f0a38b18519e15ca6928c9b0dab5e5b595dd
Eirado <GABEIRADO2@HOTMAIL.COm>
2024-04-08 10:23:15 -0300

    health bar animation(hard)

--- tanuki-the-game/tanuki-the-game/Components/HealthComponent.swift
+++ tanuki-the-game/tanuki-the-game/Components/HealthComponent.swift
@@ -8,6 +8,7 @@ class HealthComponent: GKComponent {
     var currentHealth: Float
     var healthBarNode: SCNNode?
     var receiveDamageCooldown = false
+    var lastFrame = 1
     
     init (health: Float, node: SCNNode) {
         self.maxHealth = health
@@ -16,7 +17,7 @@ class HealthComponent: GKComponent {
         super.init()
         
         
-        self.healthBarNode = createHeathBar()
+        self.healthBarNode = createHealthBar()
         if let healthBarNode = self.healthBarNode {
             node.addChildNode(healthBarNode)
         }
@@ -39,42 +40,61 @@ class HealthComponent: GKComponent {
         }
     }
     
-    func createHeathBar() -> SCNNode {
+    func createHealthBar() -> SCNNode {
         let texture = SKTexture(imageNamed: "enemy1")
-
+       
         let boxGeometry = SCNBox(width: 1, height: 0.1, length: 0, chamferRadius: 0)
-
         
         let material = SCNMaterial()
         material.diffuse.contents = texture
         
         boxGeometry.materials = [material]
         
-        let heathBarNode = SCNNode(geometry: boxGeometry)
+        let healthBarNode = SCNNode(geometry: boxGeometry)
         
         let height = node.boundingBox.max.y - node.boundingBox.min.y
-        heathBarNode.position.y = height + 0.4
+        healthBarNode.position.y = height + 0.4
         
-        return heathBarNode
+        return healthBarNode
     }
-
+    
+    func animateTexture(withTextureNamed textureName: String, frameRange: ClosedRange<Int>, duration: TimeInterval = 0.08) -> SCNAction {
+        return SCNAction.customAction(duration: duration) { node, elapsedTime in
+            let frameCount = CGFloat(frameRange.upperBound - frameRange.lowerBound + 1)
+            let normalizedTime = (elapsedTime / duration) * frameCount
+            let currentFrame = Int(normalizedTime) % Int(frameCount) + frameRange.lowerBound
+            let nextFrameTexture = SKTexture(imageNamed: String(format: "%@%d", textureName, currentFrame))
+            node.geometry?.firstMaterial?.diffuse.contents = nextFrameTexture
+        }
+    }
     
     private func updateHealthBar() {
         guard let healthBarNode = self.healthBarNode else {
             return
         }
         
-        let currentFrame = 6 + 1 - currentHealth / maxHealth * 6
+        let currentFrame = Int(6 + 1 - (currentHealth / maxHealth * 6))
+      
         
+        let animation = animateTexture(withTextureNamed: "enemy", frameRange: lastFrame...currentFrame)
         
-       
-     
+        let setTextureAction = SCNAction.run { _ in
+                let currentFrameTexture = SKTexture(imageNamed: String(format: "enemy%d", currentFrame))
+                healthBarNode.geometry?.firstMaterial?.diffuse.contents = currentFrameTexture
+            }
+        
+        healthBarNode.removeAllActions()
+        healthBarNode.runAction(.sequence([animation, setTextureAction])) {
+               self.lastFrame = currentFrame
+           }
+      
     }
     
     public func receiveDamage(damageAmount: Float) {
         
         if receiveDamageCooldown == false{
             self.currentHealth -= damageAmount
+            updateHealthBar()
         }
         
         if currentHealth <= 0 {
@@ -91,7 +111,6 @@ class HealthComponent: GKComponent {
                 ]))
             }
             
-            updateHealthBar()
             receiveDamageCooldown = true
             node.runAction(.sequence([.wait(duration: 0.5), .run({ _ in
                 self.receiveDamageCooldown = false
